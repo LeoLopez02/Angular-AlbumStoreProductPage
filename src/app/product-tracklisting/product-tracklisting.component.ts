0c17d0c6d6de7c4d816cd37b8391492b46fe2a81
Leonel López <31909505+LeoLopez02@users.noreply.github.com>
2022-02-11 20:05:23 -0400

    Update product-tracklisting.component.ts

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -10,10 +10,10 @@ import { ProductService } from "../product.service";
 export class ProductTracklistingComponent implements OnInit {
   albumInfo: Album;
 
-  constructor(private _serviceProduct: ProductService) {}
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-    this._serviceProduct
+    this._productService
       .getAlbum(1)
       .subscribe((response) => (this.albumInfo = response));
   }
